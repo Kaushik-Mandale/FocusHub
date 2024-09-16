c176e0ba737ba269934d2c6d864dba589b28949e
Kaushik-Mandale <svbkofficial@gmail.com>
2024-09-17 01:16:18 +0530

    server.js

--- server.js
+++ server.js
@@ -1,21 +1,22 @@
 const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
+const path = require('path');
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
 let sessions = {};  // To store sessions and their passwords
 
-// Serve static files from the 'public' folder
-app.use(express.static('public'));
+// Serve static files from the root directory
+app.use(express.static(path.join(__dirname)));
 
-// WebSocket handling
 io.on('connection', (socket) => {
     console.log('New user connected');
 
     // Create session with password
     socket.on('createSession', (sessionName, password) => {
+        console.log('Received createSession request:', { sessionName, password });
         if (!sessions[sessionName]) {
             sessions[sessionName] = password;
             socket.join(sessionName);
@@ -27,6 +28,7 @@ io.on('connection', (socket) => {
 
     // Join session
     socket.on('joinSession', (sessionName, password) => {
+        console.log('Received joinSession request:', { sessionName, password });
         if (sessions[sessionName] && sessions[sessionName] === password) {
             socket.join(sessionName);
             socket.emit('joinedSession', { success: true });
@@ -37,6 +39,7 @@ io.on('connection', (socket) => {
 
     // Handle chat messages
     socket.on('message', (data) => {
+        console.log('Received message:', data);
         io.to(data.sessionName).emit('newMessage', data.message);
     });
 
